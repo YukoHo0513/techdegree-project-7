371c48f8afcd119a7f1cfb8b55d28080a7636f50
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-02-04 10:37:41 -0800

    settings stored in localstorage

--- js/bell_badge.js
+++ js/bell_badge.js
@@ -1,33 +1,59 @@
 const greenDot = document.querySelector('.green-dot');
+const alertMessageList = document.querySelector('.alert-message-list');
 const bell = document.getElementById('notification');
 
-bell.addEventListener('click', ()=> {
-    bell.insertAdjacentHTML(
-        'afterbegin',
-        `<ul>
-            <li class="alert-message">
-                <div class="alert-item">
-                    <div class="green-bullet"></div>
-                    You have 6 unread messages
-                </div>
-                <span class="x-sign alert-item">&times;</span>
-            </li>
-            <li class="alert-message">
-                <div class="alert-item">
-                    <div class="green-bullet"></div>
-                    You have 3 new followers
-                </div>
-                <span class="x-sign alert-item">&times;</span>
-            </li>
-            <li class="alert-message">
-                <div class="alert-item">
-                    <div class="green-bullet"></div>
-                    Your password expires in 7 days
-                </div>
-                <span class="x-sign alert-item">&times;</span>
-            </li>
-         </ul>`
-    );
+bell.addEventListener('click', (e)=> {
+    const alertUl = alertMessageList.firstChild;
+
+    if (alertMessageList.textContent === "") {
+        greenDot.style.display = "none";
+
+        alertMessageList.innerHTML = 
+            `<ul>
+                <li class="alert-message li-1">
+                    <div class="alert-item">
+                        <div class="green-bullet"></div>
+                        You have 6 unread messages
+                    </div>
+                    <span id="x-1" class="x-sign alert-item">&times;</span>
+                </li>
+                <li class="alert-message li-2">
+                    <div class="alert-item">
+                        <div class="green-bullet"></div>
+                        You have 3 new followers
+                    </div>
+                    <span id="x-2" class="x-sign alert-item">&times;</span>
+                </li>
+                <li class="alert-message li-3">
+                    <div class="alert-item">
+                        <div class="green-bullet"></div>
+                        Your password expires in 7 days
+                    </div>
+                    <span id="x-3" class="x-sign alert-item">&times;</span>
+                </li>
+            </ul>`;
+    } 
+    else {
+        console.log(e.target);
+        if (e.target.parentElement === bell || e.target.id === "notification") {
+            console.log();
+
+            alertMessageList.innerHTML = "";
+        }
+    }
+
+    let count = 0;
+    if (e.target.className === "x-sign alert-item") {
+        e.target.parentElement.style.display = "none";
+        for (let i = 0; i < alertUl.children.length; i++) {
+            if (alertUl.children[i].style.display === "none") {
+                count += 1;
+            }
+        }
+        if (count === 3) {
+            alertUl.style.display = "none";
+        }
+    }
 });
 
 
---
+++ js/buttons.js
@@ -0,0 +1,64 @@
+const saveButton = document.getElementById('save');
+const cancelButton = document.getElementById('cancel');
+const timezone = document.getElementById('timezone');
+const checkEmail = document.getElementById('check-email');
+const checkProfile = document.getElementById('check-profile');
+
+getTimezone();
+getEmailCheck();
+getProfileCheck();
+
+// Timezone 
+function setSavedTimezone() {
+    localStorage.setItem('selectedTimezone', timezone.value);
+    return timezone.value;
+}
+function getTimezone() {
+    const timezoneInStorage = localStorage.getItem('selectedTimezone');
+    if (timezoneInStorage) {
+        timezone.value = timezoneInStorage;
+    }   
+}
+
+// Email
+function setSavedEmail() {
+    localStorage.setItem('selectedEmail', checkEmail.checked);
+}
+function getEmailCheck() {
+    const emailInStorage = localStorage.getItem('selectedEmail');
+    console.log(emailInStorage);
+    if (emailInStorage === "true") {
+        checkEmail.checked = "checked";
+    } else if (emailInStorage === "false") {
+        checkEmail.checked = "";
+    }   
+}
+
+// Profile
+function setSavedProfile() {
+    localStorage.setItem('selectedProfile', checkProfile.checked);
+}
+function getProfileCheck() {
+    const profileInStorage = localStorage.getItem('selectedProfile');
+    console.log(profileInStorage);
+
+    if (profileInStorage === "true") {
+        checkProfile.checked = "checked";
+    } else if (profileInStorage === "false") {
+        checkProfile.checked = "";
+    }
+}
+
+saveButton.addEventListener('click', (e)=> {
+    e.preventDefault();
+    setSavedTimezone();
+    setSavedEmail();
+    setSavedProfile();
+});
+
+cancelButton.addEventListener('click', ()=> {
+    timezone.value = "timezone";
+    checkEmail.checked = false;
+    checkProfile.checked = false;
+    localStorage.clear();
+});
